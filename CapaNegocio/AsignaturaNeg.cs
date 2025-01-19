4409acead0c48dae3e5487b0fe80561f95f2d0ea
CARLOS DANIEL NEACATO LLUMIQUINGA <carlos.neacato@epn.edu.ec>
2025-01-19 13:57:01 -0500

    Neg ERAprendizaje

--- CapaNegocio/AsignaturaNeg.cs
+++ CapaNegocio/AsignaturaNeg.cs
@@ -8,5 +8,6 @@ namespace CapaNegocio
 {
     public class AsignaturaNeg
     {
+
     }
 }
--- CapaNegocio/EuraceResultadoAprendizajeNeg.cs
+++ CapaNegocio/EuraceResultadoAprendizajeNeg.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using CapaEntidadea;
+
 
 namespace CapaNegocio
 {
     public class EuraceResultadoAprendizajeNeg
     {
+        private EuraceResultadoAprendizajeDAL ERAprendizaje = new EuraceResultadoAprendizajeDAL();
+
+        public void InsertarEuraceResultadoAprendizaje(EuraceResultadoAprendizaje ERApr)
+        {
+            ERAprendizaje.InsertarEuraceResultadoAprendizaje(ERApr);
+        }
+
+        public List<EuraceResultadoAprendizaje> MostrarEuraceResultadoAprendizajes()
+        {
+            return ERAprendizaje.MostrarEuraceResultadoAprendizajes();
+        }
+
+        
     }
 }
