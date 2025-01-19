505431b882d7214da157dbe62f63857c5431fcb0
Skynexito <perezkevinsan2002@gmail.com>
2025-01-19 17:02:50 -0500

    Agregando referencias  a Capa Presentacion

--- CapaPresentacion/Form1.cs
+++ CapaPresentacion/Form1.cs
@@ -9,6 +9,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Media; // Necesario para SoundPlayer.
+using CapaEntidadea;
+using CapaNegocio;
 
 
 namespace CapaPresentacion
