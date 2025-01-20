85fcaa751be6c153ca70415c6cfc977eaa74360b
JojanMC <jojangmc@gmail.com>
2025-01-20 17:00:34 -0500

    Agregacón de la Capa de Acceso a Datos

--- CapaEntidades/Carrera.cs
+++ CapaEntidades/Carrera.cs
@@ -11,7 +11,7 @@ namespace CapaEntidadea
         public int Id { get; set; }
         public string Nombre { get; set; }
         public string Contraseña { get; set; }
-        public string Presum { get; set; }
+        public string Pensum { get; set; }
 
         // Relación con CarreraAsignatura (Muchos a Muchos con Asignatura)
         public ICollection<CarreraAsignatura> CarrerasAsignatura { get; set; }
