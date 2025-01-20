3f53d5670d33d8bcf6df1167f840b02565a720d4
Skynexito <perezkevinsan2002@gmail.com>
2025-01-20 17:15:27 -0500

    Modificaicon movimiento form asignatura

--- CapaPresentacion/CRUD/FormAsignatura.Designer.cs
+++ CapaPresentacion/CRUD/FormAsignatura.Designer.cs
@@ -52,18 +52,22 @@
             // 
             // guna2CustomGradientPanel1
             // 
-            this.guna2CustomGradientPanel1.Controls.Add(this.guna2CirclePictureBox2);
             this.guna2CustomGradientPanel1.Controls.Add(this.guna2CirclePictureBox1);
-            this.guna2CustomGradientPanel1.Controls.Add(this.lblAccionAsignatura);
             this.guna2CustomGradientPanel1.Controls.Add(this.lblSalir);
+            this.guna2CustomGradientPanel1.Controls.Add(this.lblAccionAsignatura);
+            this.guna2CustomGradientPanel1.Controls.Add(this.guna2CirclePictureBox2);
             this.guna2CustomGradientPanel1.FillColor = System.Drawing.Color.RoyalBlue;
             this.guna2CustomGradientPanel1.FillColor2 = System.Drawing.Color.RoyalBlue;
             this.guna2CustomGradientPanel1.FillColor3 = System.Drawing.Color.Navy;
             this.guna2CustomGradientPanel1.FillColor4 = System.Drawing.Color.DarkBlue;
-            this.guna2CustomGradientPanel1.Location = new System.Drawing.Point(-10, -10);
+            this.guna2CustomGradientPanel1.Location = new System.Drawing.Point(-13, -12);
+            this.guna2CustomGradientPanel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.guna2CustomGradientPanel1.Name = "guna2CustomGradientPanel1";
-            this.guna2CustomGradientPanel1.Size = new System.Drawing.Size(265, 30);
+            this.guna2CustomGradientPanel1.Size = new System.Drawing.Size(353, 37);
             this.guna2CustomGradientPanel1.TabIndex = 20;
+            this.guna2CustomGradientPanel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.guna2CustomGradientPanel1_MouseDown);
+            this.guna2CustomGradientPanel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.guna2CustomGradientPanel1_MouseMove);
+            this.guna2CustomGradientPanel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.guna2CustomGradientPanel1_MouseUp);
             // 
             // guna2CirclePictureBox2
             // 
@@ -71,10 +75,11 @@
             this.guna2CirclePictureBox2.FillColor = System.Drawing.Color.Transparent;
             this.guna2CirclePictureBox2.Image = global::CapaPresentacion.Properties.Resources.BB;
             this.guna2CirclePictureBox2.ImageRotate = 0F;
-            this.guna2CirclePictureBox2.Location = new System.Drawing.Point(11, 10);
+            this.guna2CirclePictureBox2.Location = new System.Drawing.Point(15, 12);
+            this.guna2CirclePictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.guna2CirclePictureBox2.Name = "guna2CirclePictureBox2";
             this.guna2CirclePictureBox2.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
-            this.guna2CirclePictureBox2.Size = new System.Drawing.Size(16, 20);
+            this.guna2CirclePictureBox2.Size = new System.Drawing.Size(21, 25);
             this.guna2CirclePictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.guna2CirclePictureBox2.TabIndex = 21;
             this.guna2CirclePictureBox2.TabStop = false;
@@ -83,12 +88,14 @@
             // guna2CirclePictureBox1
             // 
             this.guna2CirclePictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2CirclePictureBox1.Enabled = false;
             this.guna2CirclePictureBox1.FillColor = System.Drawing.Color.Transparent;
             this.guna2CirclePictureBox1.ImageRotate = 0F;
-            this.guna2CirclePictureBox1.Location = new System.Drawing.Point(9, 7);
+            this.guna2CirclePictureBox1.Location = new System.Drawing.Point(12, 9);
+            this.guna2CirclePictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.guna2CirclePictureBox1.Name = "guna2CirclePictureBox1";
             this.guna2CirclePictureBox1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
-            this.guna2CirclePictureBox1.Size = new System.Drawing.Size(16, 20);
+            this.guna2CirclePictureBox1.Size = new System.Drawing.Size(21, 25);
             this.guna2CirclePictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.guna2CirclePictureBox1.TabIndex = 11;
             this.guna2CirclePictureBox1.TabStop = false;
@@ -98,11 +105,13 @@
             // 
             this.lblAccionAsignatura.AutoSize = true;
             this.lblAccionAsignatura.BackColor = System.Drawing.Color.Transparent;
+            this.lblAccionAsignatura.Enabled = false;
             this.lblAccionAsignatura.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblAccionAsignatura.ForeColor = System.Drawing.Color.White;
-            this.lblAccionAsignatura.Location = new System.Drawing.Point(27, 12);
+            this.lblAccionAsignatura.Location = new System.Drawing.Point(36, 15);
+            this.lblAccionAsignatura.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblAccionAsignatura.Name = "lblAccionAsignatura";
-            this.lblAccionAsignatura.Size = new System.Drawing.Size(98, 18);
+            this.lblAccionAsignatura.Size = new System.Drawing.Size(121, 20);
             this.lblAccionAsignatura.TabIndex = 8;
             this.lblAccionAsignatura.Text = "Crear asignatura";
             // 
@@ -112,9 +121,10 @@
             this.lblSalir.BackColor = System.Drawing.Color.Transparent;
             this.lblSalir.Font = new System.Drawing.Font("Agency FB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblSalir.ForeColor = System.Drawing.SystemColors.Control;
-            this.lblSalir.Location = new System.Drawing.Point(245, 7);
+            this.lblSalir.Location = new System.Drawing.Point(327, 9);
+            this.lblSalir.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblSalir.Name = "lblSalir";
-            this.lblSalir.Size = new System.Drawing.Size(16, 20);
+            this.lblSalir.Size = new System.Drawing.Size(18, 24);
             this.lblSalir.TabIndex = 1;
             this.lblSalir.Text = "x";
             this.lblSalir.Click += new System.EventHandler(this.lblSalir_Click);
@@ -132,9 +142,10 @@
             this.btnCancelar.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
             this.btnCancelar.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.btnCancelar.ForeColor = System.Drawing.Color.White;
-            this.btnCancelar.Location = new System.Drawing.Point(132, 192);
+            this.btnCancelar.Location = new System.Drawing.Point(176, 236);
+            this.btnCancelar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnCancelar.Name = "btnCancelar";
-            this.btnCancelar.Size = new System.Drawing.Size(75, 28);
+            this.btnCancelar.Size = new System.Drawing.Size(100, 34);
             this.btnCancelar.TabIndex = 19;
             this.btnCancelar.Text = "Cancelar";
             this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
@@ -151,9 +162,10 @@
             this.btnCrear.FillColor2 = System.Drawing.Color.MidnightBlue;
             this.btnCrear.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.btnCrear.ForeColor = System.Drawing.Color.White;
-            this.btnCrear.Location = new System.Drawing.Point(35, 192);
+            this.btnCrear.Location = new System.Drawing.Point(47, 236);
+            this.btnCrear.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnCrear.Name = "btnCrear";
-            this.btnCrear.Size = new System.Drawing.Size(75, 28);
+            this.btnCrear.Size = new System.Drawing.Size(100, 34);
             this.btnCrear.TabIndex = 18;
             this.btnCrear.Text = "Crear";
             this.btnCrear.Click += new System.EventHandler(this.btnCrear_Click);
@@ -162,9 +174,10 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(28, 140);
+            this.label3.Location = new System.Drawing.Point(37, 172);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(53, 22);
+            this.label3.Size = new System.Drawing.Size(66, 26);
             this.label3.TabIndex = 14;
             this.label3.Text = "Nivel:";
             // 
@@ -172,9 +185,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(9, 105);
+            this.label2.Location = new System.Drawing.Point(12, 129);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(72, 22);
+            this.label2.Size = new System.Drawing.Size(90, 26);
             this.label2.TabIndex = 13;
             this.label2.Text = "Nombre:";
             // 
@@ -182,9 +196,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(15, 71);
+            this.label1.Location = new System.Drawing.Point(20, 87);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(66, 22);
+            this.label1.Size = new System.Drawing.Size(80, 26);
             this.label1.TabIndex = 12;
             this.label1.Text = "Codigo:";
             // 
@@ -192,9 +207,10 @@
             // 
             this.lblAsignatura.AutoSize = true;
             this.lblAsignatura.Font = new System.Drawing.Font("Trebuchet MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblAsignatura.Location = new System.Drawing.Point(66, 30);
+            this.lblAsignatura.Location = new System.Drawing.Point(88, 37);
+            this.lblAsignatura.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblAsignatura.Name = "lblAsignatura";
-            this.lblAsignatura.Size = new System.Drawing.Size(111, 27);
+            this.lblAsignatura.Size = new System.Drawing.Size(144, 35);
             this.lblAsignatura.TabIndex = 11;
             this.lblAsignatura.Text = "Asignatura";
             this.lblAsignatura.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -217,13 +233,13 @@
             this.tbCodigo.Font = new System.Drawing.Font("Californian FB", 9F);
             this.tbCodigo.ForeColor = System.Drawing.Color.Black;
             this.tbCodigo.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.tbCodigo.Location = new System.Drawing.Point(81, 71);
+            this.tbCodigo.Location = new System.Drawing.Point(108, 87);
             this.tbCodigo.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
             this.tbCodigo.Name = "tbCodigo";
             this.tbCodigo.PasswordChar = '\0';
             this.tbCodigo.PlaceholderText = "FIEED100";
             this.tbCodigo.SelectedText = "";
-            this.tbCodigo.Size = new System.Drawing.Size(153, 22);
+            this.tbCodigo.Size = new System.Drawing.Size(204, 27);
             this.tbCodigo.Style = Guna.UI2.WinForms.Enums.TextBoxStyle.Material;
             this.tbCodigo.TabIndex = 21;
             this.tbCodigo.Enter += new System.EventHandler(this.tbCodigo_Enter);
@@ -242,13 +258,13 @@
             this.tbNombre.Font = new System.Drawing.Font("Californian FB", 9F);
             this.tbNombre.ForeColor = System.Drawing.Color.Black;
             this.tbNombre.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.tbNombre.Location = new System.Drawing.Point(81, 105);
+            this.tbNombre.Location = new System.Drawing.Point(108, 129);
             this.tbNombre.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
             this.tbNombre.Name = "tbNombre";
             this.tbNombre.PasswordChar = '\0';
             this.tbNombre.PlaceholderText = "MATERIA";
             this.tbNombre.SelectedText = "";
-            this.tbNombre.Size = new System.Drawing.Size(153, 22);
+            this.tbNombre.Size = new System.Drawing.Size(204, 27);
             this.tbNombre.Style = Guna.UI2.WinForms.Enums.TextBoxStyle.Material;
             this.tbNombre.TabIndex = 22;
             this.tbNombre.Enter += new System.EventHandler(this.tbNombre_Enter);
@@ -267,13 +283,13 @@
             this.tbNivel.Font = new System.Drawing.Font("Californian FB", 9F);
             this.tbNivel.ForeColor = System.Drawing.Color.Black;
             this.tbNivel.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.tbNivel.Location = new System.Drawing.Point(81, 140);
+            this.tbNivel.Location = new System.Drawing.Point(108, 172);
             this.tbNivel.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
             this.tbNivel.Name = "tbNivel";
             this.tbNivel.PasswordChar = '\0';
             this.tbNivel.PlaceholderText = "1, 2, 3, 4, 5, 6, 7, 8, 9";
             this.tbNivel.SelectedText = "";
-            this.tbNivel.Size = new System.Drawing.Size(153, 22);
+            this.tbNivel.Size = new System.Drawing.Size(204, 27);
             this.tbNivel.Style = Guna.UI2.WinForms.Enums.TextBoxStyle.Material;
             this.tbNivel.TabIndex = 23;
             this.tbNivel.Enter += new System.EventHandler(this.tbNivel_Enter);
@@ -284,29 +300,31 @@
             this.lbAdvertencia.AutoSize = true;
             this.lbAdvertencia.Font = new System.Drawing.Font("Trebuchet MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbAdvertencia.ForeColor = System.Drawing.Color.Red;
-            this.lbAdvertencia.Location = new System.Drawing.Point(38, 171);
+            this.lbAdvertencia.Location = new System.Drawing.Point(51, 210);
+            this.lbAdvertencia.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbAdvertencia.Name = "lbAdvertencia";
-            this.lbAdvertencia.Size = new System.Drawing.Size(172, 16);
+            this.lbAdvertencia.Size = new System.Drawing.Size(220, 18);
             this.lbAdvertencia.TabIndex = 24;
             this.lbAdvertencia.Text = "Debe completar todos los campos";
             // 
             // FormAsignatura
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(253, 230);
+            this.ClientSize = new System.Drawing.Size(337, 283);
             this.Controls.Add(this.lbAdvertencia);
             this.Controls.Add(this.tbNivel);
             this.Controls.Add(this.tbNombre);
             this.Controls.Add(this.tbCodigo);
-            this.Controls.Add(this.guna2CustomGradientPanel1);
             this.Controls.Add(this.btnCancelar);
             this.Controls.Add(this.btnCrear);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lblAsignatura);
+            this.Controls.Add(this.guna2CustomGradientPanel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormAsignatura";
             this.Text = "FormAsignatura";
             this.guna2CustomGradientPanel1.ResumeLayout(false);
