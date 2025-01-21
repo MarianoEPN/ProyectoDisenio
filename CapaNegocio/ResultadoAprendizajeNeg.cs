cd7801c31661663c5e309776c925c4be98c40fb1
MARIANO GABRIEL ZAMBRANO TUFINO <mariano.zambrano@epn.edu.ec>
2025-01-21 06:55:18 -0500

    Capa de negocio resultados de aprendizaje solamente crud

--- CapaNegocio/ResultadoAprendizajeNeg.cs
+++ CapaNegocio/ResultadoAprendizajeNeg.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CapaEntidadea;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,25 @@ namespace CapaNegocio
 {
     public class ResultadoAprendizajeNeg
     {
+        private ResultadoAprendizajeNeg RANeg = new ResultadoAprendizajeNeg();
+
+        /*
+         public void InsertarResultadoAprendizajeNeg(ResultadoAprendizajeNeg MRApr)
+         {
+             RANeg.InsertarResultadoAprendizajeNeg(MRApr);
+         }
+         public List<MatchResultadoAprendizaje> MostrarResultadoAprendizajeNeg()
+         {
+              return RANeg.MostrarResultadoAprendizajeNeg();
+         }
+         public void ActualizarResultadoAprendizajeNeg(ResultadoAprendizajeNeg MRApr)
+         {
+              RANeg.ActualizarResultadoAprendizajeNeg(MRApr);
+         }
+         public void EliminarResultadoAprendizajeNeg(int id)
+         {
+              RANeg.EliminarResultadoAprendizajeNeg(id);
+         }
+        */
     }
 }
--- CapaNegocio/TipoResultadoAsignaturaNeg.cs
+++ CapaNegocio/TipoResultadoAsignaturaNeg.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CapaAccesoDatos;
+using CapaEntidadea;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,22 @@ namespace CapaNegocio
 {
     public class TipoResultadoAsignaturaNeg
     {
+        TipoResultadoAsignaturaDAL TRAsignatura = new TipoResultadoAsignaturaDAL();
+        public void InsertarMatchResultadoAprendizaje(TipoResultadoAsignatura TRAsig)
+        {
+            TRAsignatura.InsertarTipoResultadoAsignatura(TRAsig);
+        }
+        public List<TipoResultadoAsignatura> MostrarMatchResultadoAprendizaje()
+        {
+            return TRAsignatura.MostrarTipoResultadoAsignatura();
+        }
+        public void ActualizarMatchResultadoAprendizaje(TipoResultadoAsignatura TRAsig)
+        {
+            TRAsignatura.ActualizarTipoResultadoAsignatura(TRAsig);
+        }
+        public void EliminarMatchResultadoAprendizaje(int id)
+        {
+            TRAsignatura.EliminarTipoResultadoAsignatura(id);
+        }
     }
 }
