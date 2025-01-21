a33ef509b1af014b20c327d7dbaf9e03ea1253a9
Skynexito <perezkevinsan2002@gmail.com>
2025-01-20 21:51:58 -0500

    Implementacion de formulario menu MDI

---
+++ CapaPresentacion/Bienvenido.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace CapaPresentacion
+{
+    partial class Bienvenido
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.guna2ShadowPanel2 = new Guna.UI2.WinForms.Guna2ShadowPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.guna2ShadowPanel1 = new Guna.UI2.WinForms.Guna2ShadowPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.guna2ShadowPanel2.SuspendLayout();
+            this.guna2ShadowPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // guna2ShadowPanel2
+            // 
+            this.guna2ShadowPanel2.BackColor = System.Drawing.Color.Transparent;
+            this.guna2ShadowPanel2.Controls.Add(this.pictureBox1);
+            this.guna2ShadowPanel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.guna2ShadowPanel2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(31)))), ((int)(((byte)(62)))));
+            this.guna2ShadowPanel2.Location = new System.Drawing.Point(0, 443);
+            this.guna2ShadowPanel2.Name = "guna2ShadowPanel2";
+            this.guna2ShadowPanel2.Radius = 10;
+            this.guna2ShadowPanel2.ShadowColor = System.Drawing.Color.Black;
+            this.guna2ShadowPanel2.Size = new System.Drawing.Size(1029, 186);
+            this.guna2ShadowPanel2.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Californian FB", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(40)))), ((int)(((byte)(58)))));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(217, 50);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Welcome!!";
+            // 
+            // guna2ShadowPanel1
+            // 
+            this.guna2ShadowPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ShadowPanel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2ShadowPanel1.Controls.Add(this.pictureBox2);
+            this.guna2ShadowPanel1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(40)))), ((int)(((byte)(58)))));
+            this.guna2ShadowPanel1.Location = new System.Drawing.Point(21, 62);
+            this.guna2ShadowPanel1.Name = "guna2ShadowPanel1";
+            this.guna2ShadowPanel1.Radius = 10;
+            this.guna2ShadowPanel1.ShadowColor = System.Drawing.Color.Black;
+            this.guna2ShadowPanel1.Size = new System.Drawing.Size(988, 375);
+            this.guna2ShadowPanel1.TabIndex = 9;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.pictureBox1.Image = global::CapaPresentacion.Properties.Resources.info;
+            this.pictureBox1.Location = new System.Drawing.Point(177, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(684, 155);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox2.Image = global::CapaPresentacion.Properties.Resources.Welcome;
+            this.pictureBox2.Location = new System.Drawing.Point(36, 20);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(933, 338);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Bienvenido
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1029, 629);
+            this.Controls.Add(this.guna2ShadowPanel2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.guna2ShadowPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Bienvenido";
+            this.Text = "Bienvenido";
+            this.guna2ShadowPanel2.ResumeLayout(false);
+            this.guna2ShadowPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2ShadowPanel guna2ShadowPanel2;
+        private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2ShadowPanel guna2ShadowPanel1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ CapaPresentacion/Menuu.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaPresentacion
+{
+    public partial class Menuu : Form
+    {
+        bool sidebarExpanded;
+        public Menuu()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Menuu_Load(object sender, EventArgs e)
+        {
+            panelOpciones.Visible = false;
+
+        }
+
+        private void timerExpanded_Tick(object sender, EventArgs e)
+        {
+            if (sidebarExpanded)
+            {
+                panelBarra.Width -= 10;
+                if (panelBarra.Width == panelBarra.MinimumSize.Width)
+                {
+                    sidebarExpanded = false;
+                    timerExpanded.Stop();
+                }
+            }
+            else 
+            {
+                panelBarra.Width += 10;
+                if (panelBarra.Width == panelBarra.MaximumSize.Width)
+                {
+                    sidebarExpanded = true;
+                    timerExpanded.Stop();
+                }
+
+            }
+        }
+
+        private void btnMenu_Click(object sender, EventArgs e)
+        {
+            timerExpanded.Start();
+        }
+
+        private void container(object _form)
+        {
+            if (panelContainer.Controls.Count > 0) panelContainer.Controls.Clear();
+            Form fm = _form as Form;
+            fm.TopLevel = false;
+            fm.FormBorderStyle = FormBorderStyle.None;
+            fm.Dock = DockStyle.Fill;
+            panelContainer.Controls.Add(fm);
+            panelContainer.Tag = fm;
+            fm.Show(); 
+        }
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            container(new Bienvenido());
+            lblTitulo.Text = "Home";
+        }
+
+        private void panelContainer_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnBajar_Click(object sender, EventArgs e)
+        {
+            if
+                (!panelOpciones.Visible)
+            {
+                panelOpciones.Visible = true;
+                btnBajar.BackgroundImage = Properties.Resources.up__2_;
+            }
+
+            else
+            {
+                panelOpciones.Visible = false;
+                btnBajar.BackgroundImage = Properties.Resources.baja;
+
+            }
+        }
+
+
+    }
+}
