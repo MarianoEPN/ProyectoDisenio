b99a2cfdf6ac8b04c6e2f775d1267decf353fffb
FaBrii-ID97 <fabricio.tixe@epn.edu.ec>
2025-01-21 13:43:58 -0500

    Se agregra CarreraDAL y AsignaturaDAL

---
+++ CapaAccesoDatos/AsignaturaDAL.cs
@@ -0,0 +1,87 @@
+﻿using CapaEntidades;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaAccesoDatos
+{
+    public class AsignaturaDAL
+    {
+        private ConexionBD conexion = new ConexionBD();
+        SqlDataReader leer;
+        SqlCommand comando = new SqlCommand();
+
+        // Mostrar todas las asignaturas
+        public List<Asignatura> MostrarAsignaturas()
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "MostrarAsignatura";
+            comando.CommandType = System.Data.CommandType.StoredProcedure;
+            comando.Parameters.Clear();
+            leer = comando.ExecuteReader();
+
+            List<Asignatura> lista = new List<Asignatura>();
+
+            while (leer.Read())
+            {
+                Asignatura asignatura = new Asignatura();
+                asignatura.Id = leer.GetInt32(0);
+                asignatura.Codigo = leer.GetString(1);
+                asignatura.Nombre = leer.GetString(2);
+                asignatura.Nivel = leer.GetInt32(3);
+                lista.Add(asignatura);
+            }
+            leer.Close();
+            conexion.CerrarConexion();
+            return lista;
+        }
+
+        // Insertar una nueva asignatura
+        public void InsertarAsignatura(Asignatura asignatura)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "InsertarAsignatura";
+            comando.CommandType = System.Data.CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@id", asignatura.Id);
+            comando.Parameters.AddWithValue("@codigo", asignatura.Codigo);
+            comando.Parameters.AddWithValue("@nombre", asignatura.Nombre);
+            comando.Parameters.AddWithValue("@nivel", asignatura.Nivel);
+
+            comando.ExecuteNonQuery();
+            comando.Parameters.Clear();
+            conexion.CerrarConexion();
+        }
+
+        // Actualizar una asignatura existente
+        public void ActualizarAsignatura(Asignatura asignatura)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "ActualizarAsignatura";
+            comando.CommandType = System.Data.CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@id", asignatura.Id);
+            comando.Parameters.AddWithValue("@codigo", asignatura.Codigo);
+            comando.Parameters.AddWithValue("@nombre", asignatura.Nombre);
+            comando.Parameters.AddWithValue("@nivel", asignatura.Nivel);
+
+            comando.ExecuteNonQuery();
+            comando.Parameters.Clear();
+            conexion.CerrarConexion();
+        }
+
+        // Eliminar una asignatura por ID
+        public void EliminarAsignatura(int id)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "EliminarAsignatura";
+            comando.CommandType = System.Data.CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@id", id);
+
+            comando.ExecuteNonQuery();
+            comando.Parameters.Clear();
+            conexion.CerrarConexion();
+        }
+    }
+}
---
+++ CapaAccesoDatos/CarreraDAL.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using CapaEntidades;
+
+namespace CapaAccesoDatos
+{
+    public class CarreraDAL
+    {
+        private ConexionBD conexion = new ConexionBD();
+        SqlDataReader leer;
+        SqlCommand comando = new SqlCommand();
+
+        // Mostrar todas las carreras
+        public List<Carrera> MostrarCarrera()
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "MostrarCarrera";
+            comando.CommandType = System.Data.CommandType.StoredProcedure;
+            comando.Parameters.Clear();
+            leer = comando.ExecuteReader();
+
+            List<Carrera> lista = new List<Carrera>();
+
+            while (leer.Read())
+            {
+                Carrera carrera = new Carrera();
+                carrera.Id = leer.GetInt32(0);
+                carrera.Correo = leer.GetString(1);
+                carrera.Nombre = leer.GetString(2);
+                carrera.Contraseña = leer.GetString(3);
+                carrera.Presum = leer.GetString(4);
+                lista.Add(carrera);
+            }
+            leer.Close();
+            conexion.CerrarConexion();
+            return lista;
+        }
+
+        // Insertar una nueva carrera
+        public void InsertarCarrera(Carrera carrera)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "InsertarCarrera";
+            comando.CommandType = System.Data.CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@id", carrera.Id);
+            comando.Parameters.AddWithValue("@correo", carrera.Correo);
+            comando.Parameters.AddWithValue("@nombre", carrera.Nombre);
+            comando.Parameters.AddWithValue("@contrasenia", carrera.Contraseña);
+            comando.Parameters.AddWithValue("@pensum", carrera.Presum);
+
+            comando.ExecuteNonQuery();
+            comando.Parameters.Clear();
+            conexion.CerrarConexion();
+        }
+
+        // Actualizar una carrera existente
+        public void ActualizarCarrera(Carrera carrera)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "ActualizarCarrera";
+            comando.CommandType = System.Data.CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@id", carrera.Id);
+            comando.Parameters.AddWithValue("@correo", carrera.Correo);
+            comando.Parameters.AddWithValue("@nombre", carrera.Nombre);
+            comando.Parameters.AddWithValue("@contrasenia", carrera.Contraseña);
+            comando.Parameters.AddWithValue("@pensum", carrera.Presum);
+
+            comando.ExecuteNonQuery();
+            comando.Parameters.Clear();
+            conexion.CerrarConexion();
+        }
+
+        // Eliminar una carrera por ID
+        public void EliminarCarrera(int id)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "EliminarCarrera";
+            comando.CommandType = System.Data.CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@id", id);
+
+            comando.ExecuteNonQuery();
+            comando.Parameters.Clear();
+            conexion.CerrarConexion();
+        }
+    }
+}
