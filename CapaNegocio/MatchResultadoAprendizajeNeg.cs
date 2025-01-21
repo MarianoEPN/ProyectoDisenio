cafa8d670297fe3c9c65134fb1b372df844c85c0
CESAR DAVID MOROCHO CHISAGUANO <cesar.morocho@epn.edu.ec>
2025-01-20 21:56:47 -0500

    Negocio: EuraceResAprend, EuraceResASig, MatchResApren

--- CapaNegocio/MatchResultadoAprendizajeNeg.cs
+++ CapaNegocio/MatchResultadoAprendizajeNeg.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CapaAccesoDatos;
+using CapaEntidadea;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,23 @@ namespace CapaNegocio
 {
     public class MatchResultadoAprendizajeNeg
     {
+        private MatchResultadoAprendizajeDAL MRAprendizaje = new MatchResultadoAprendizajeDAL();
+
+        public void InsertarMatchResultadoAprendizaje(MatchResultadoAprendizaje MRApr)
+        {
+            MRAprendizaje.InsertarMatchResultadoAprendizaje(MRApr);
+        }
+        public List<MatchResultadoAprendizaje> MostrarMatchResultadoAprendizaje()
+        {
+            return MRAprendizaje.MostrarMatchResultadoAprendizaje();
+        }
+        public void ActualizarMatchResultadoAprendizaje(MatchResultadoAprendizaje MRApr)
+        {
+            MRAprendizaje.ActualizarMatchResultadoAprendizaje(MRApr);
+        }
+        public void EliminarMatchResultadoAprendizaje(int id)
+        {
+            MRAprendizaje.EliminarMatchResultadoAprendizaje(id);
+        }
     }
 }
