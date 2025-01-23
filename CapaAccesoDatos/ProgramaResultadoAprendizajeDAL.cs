50d87bb0067948cb476b3111561e2fe879557134
JojanMC <jojangmc@gmail.com>
2025-01-23 10:08:42 -0500

    Actualizacion de capa

--- CapaAccesoDatos/ProgramaResultadoAprendizajeDAL.cs
+++ CapaAccesoDatos/ProgramaResultadoAprendizajeDAL.cs
@@ -14,56 +14,53 @@ namespace CapaAccesoDatos
         SqlDataReader leer;
         SqlCommand comando = new SqlCommand();
 
-        public List<ProgramaResultadoAprend> MostrarProgramaResultadoAprendizaje()
+        public List<ProgramaResultadoAprendizaje> MostrarProgramaResultadoAprendizaje()
         {
             comando.Connection = conexion.AbrirConexion();
             comando.CommandText = "MostrarProgramaResultadoAprendizaje";
             comando.CommandType = System.Data.CommandType.StoredProcedure;
+            comando.Parameters.Clear();
             leer = comando.ExecuteReader();
 
-            List<ProgramaResultadoAprend> lista = new List<ProgramaResultadoAprend>();
+            List<ProgramaResultadoAprendizaje> lista = new List<ProgramaResultadoAprendizaje>();
 
             while (leer.Read())
             {
-                ProgramaResultadoAprend programaResultadoAprendizaje = new ProgramaResultadoAprend();
-                programaResultadoAprendizaje.Id = leer.GetInt32(0);
-                programaResultadoAprendizaje.Comentario = leer.GetString(1);
-                programaResultadoAprendizaje.ObjProgramaId = leer.GetInt32(2);
-                programaResultadoAprendizaje.ResultadoAprendizajeId = leer.GetInt32(3);
-
-                lista.Add(programaResultadoAprendizaje);
-
+                ProgramaResultadoAprendizaje item = new ProgramaResultadoAprendizaje();
+                item.Id = leer.GetInt32(0);
+                item.ObjProgramaId = leer.GetInt32(1);
+                item.ResultadoAprendizajeId = leer.GetInt32(2);
+                item.Comentario = leer.GetString(3);
+                lista.Add(item);
             }
+
             leer.Close();
             conexion.CerrarConexion();
             return lista;
         }
 
-        public void InsertarProgramaResultadoAprendizaje(ProgramaResultadoAprend result)
+        public void InsertarProgramaResultadoAprendizaje(ProgramaResultadoAprendizaje item)
         {
             comando.Connection = conexion.AbrirConexion();
             comando.CommandText = "InsertarProgramaResultadoAprendizaje";
             comando.CommandType = System.Data.CommandType.StoredProcedure;
-            comando.Parameters.AddWithValue("@comentario", result.Comentario);
-            comando.Parameters.AddWithValue("@obj_programa_id", result.ObjProgramaId);
-            comando.Parameters.AddWithValue("@resultado_aprendizaje_id", result.ResultadoAprendizajeId);
-
+            comando.Parameters.AddWithValue("@obj_programa_id", item.ObjProgramaId);
+            comando.Parameters.AddWithValue("@resultado_aprendizaje_id", item.ResultadoAprendizajeId);
+            comando.Parameters.AddWithValue("@comentario", item.Comentario);
             comando.ExecuteNonQuery();
             comando.Parameters.Clear();
             conexion.CerrarConexion();
-
         }
 
-        public void ActualizarProgramaResultadoAprendizaje(ProgramaResultadoAprend result)
+        public void ActualizarProgramaResultadoAprendizaje(ProgramaResultadoAprendizaje item)
         {
             comando.Connection = conexion.AbrirConexion();
             comando.CommandText = "ActualizarProgramaResultadoAprendizaje";
             comando.CommandType = System.Data.CommandType.StoredProcedure;
-            comando.Parameters.AddWithValue("@id", result.Id);
-            comando.Parameters.AddWithValue("@comentario", result.Comentario);
-            comando.Parameters.AddWithValue("@obj_programa_id", result.ObjProgramaId);
-            comando.Parameters.AddWithValue("@resultado_aprendizaje_id", result.ResultadoAprendizajeId);
-
+            comando.Parameters.AddWithValue("@id", item.Id);
+            comando.Parameters.AddWithValue("@obj_programa_id", item.ObjProgramaId);
+            comando.Parameters.AddWithValue("@resultado_aprendizaje_id", item.ResultadoAprendizajeId);
+            comando.Parameters.AddWithValue("@comentario", item.Comentario);
             comando.ExecuteNonQuery();
             comando.Parameters.Clear();
             conexion.CerrarConexion();
---
+++ CapaAccesoDatos/ResultadoAprendizajeAsignaturaDAL.cs
@@ -0,0 +1,83 @@
+﻿using CapaEntidades;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaAccesoDatos
+{
+    public class ResultadoAprendizajeAsignaturaDAL
+    {
+        private ConexionBD conexion = new ConexionBD();
+        SqlDataReader leer;
+        SqlCommand comando = new SqlCommand();
+
+        public List<ResultadoAprendizajeAsignatura> MostrarResultadoAprendizajeAsignatura()
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "MostrarResultadoAprendizajeAsignatura";
+            comando.CommandType = System.Data.CommandType.StoredProcedure;
+            comando.Parameters.Clear();
+            leer = comando.ExecuteReader();
+
+            List<ResultadoAprendizajeAsignatura> lista = new List<ResultadoAprendizajeAsignatura>();
+
+            while (leer.Read())
+            {
+                ResultadoAprendizajeAsignatura item = new ResultadoAprendizajeAsignatura();
+                item.Id = leer.GetInt32(0);
+                item.AsignaturaId = leer.GetInt32(1);
+                item.TipoId = leer.GetInt32(2);
+                item.Codigo = leer.GetString(3);
+                item.Descripcion = leer.GetString(4);
+                lista.Add(item);
+            }
+
+            leer.Close();
+            conexion.CerrarConexion();
+            return lista;
+        }
+
+        public void InsertarResultadoAprendizajeAsignatura(ResultadoAprendizajeAsignatura item)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "InsertarResultadoAprendizajeAsignatura";
+            comando.CommandType = System.Data.CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@asignatura_id", item.AsignaturaId);
+            comando.Parameters.AddWithValue("@tipo_id", item.TipoId);
+            comando.Parameters.AddWithValue("@codigo", item.Codigo);
+            comando.Parameters.AddWithValue("@descripcion", item.Descripcion);
+            comando.ExecuteNonQuery();
+            comando.Parameters.Clear();
+            conexion.CerrarConexion();
+        }
+
+        public void ActualizarResultadoAprendizajeAsignatura(ResultadoAprendizajeAsignatura item)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "ActualizarResultadoAprendizajeAsignatura";
+            comando.CommandType = System.Data.CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@id", item.Id);
+            comando.Parameters.AddWithValue("@asignatura_id", item.AsignaturaId);
+            comando.Parameters.AddWithValue("@tipo_id", item.TipoId);
+            comando.Parameters.AddWithValue("@codigo", item.Codigo);
+            comando.Parameters.AddWithValue("@descripcion", item.Descripcion);
+            comando.ExecuteNonQuery();
+            comando.Parameters.Clear();
+            conexion.CerrarConexion();
+        }
+
+        public void EliminarResultadoAprendizajeAsignatura(int id)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "EliminarResultadoAprendizajeAsignatura";
+            comando.CommandType = System.Data.CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@id", id);
+            comando.ExecuteNonQuery();
+            comando.Parameters.Clear();
+            conexion.CerrarConexion();
+        }
+    }
+}
