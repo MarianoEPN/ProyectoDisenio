053552b2300868dd0d709d8bf996cdd5b303c511
Skynexito <perezkevinsan2002@gmail.com>
2025-01-28 10:05:41 -0500

    Modificando opcion editar usuario de capa presentacion

--- CapaPresentacion/MenuOpciones/FormEditarUsuario.Designer.cs
+++ CapaPresentacion/MenuOpciones/FormEditarUsuario.Designer.cs
@@ -178,7 +178,7 @@
             this.label1.ForeColor = System.Drawing.Color.DimGray;
             this.label1.Location = new System.Drawing.Point(69, 350);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(166, 39);
+            this.label1.Size = new System.Drawing.Size(156, 38);
             this.label1.TabIndex = 45;
             this.label1.Text = "Username";
             // 
@@ -394,7 +394,7 @@
             this.label3.ForeColor = System.Drawing.Color.DimGray;
             this.label3.Location = new System.Drawing.Point(386, 152);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(130, 39);
+            this.label3.Size = new System.Drawing.Size(124, 38);
             this.label3.TabIndex = 50;
             this.label3.Text = "Carrera";
             // 
